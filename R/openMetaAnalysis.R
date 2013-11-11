4e5eaa065fda4396805199161f5093fb69bd0ada
badgettrg <rbadgett@kumc.edu>
2013-11-10 22:38:31 -0600

    Update openMetaAnalysis.R

--- R/openMetaAnalysis.R
+++ R/openMetaAnalysis.R
@@ -1,4 +1,4 @@
-openMetaAnalysis <- function(content,lefthand,righthand, type, topic, theme, package) {
+openMetaAnalysis <- function(content,lefthand,righthand, type, cofactorlabel, topic, theme) {
 temp <- content
 # http://stat.ethz.ch/R-manual/R-devel/library/base/html/regex.html
 temp <- gsub('\n', '', fixed = TRUE, temp, perl = TRUE)
@@ -21,33 +21,40 @@ attach(myframe)
 KUBlue = "#0022B4"
 SkyBlue = "#6DC6E7"
 #par(col.axis="black" ,col.lab=KUBlue ,col.main=KUBlue ,col.sub=KUBlue, col=KUBlue,new = TRUE) #bg=SkyBlue)
-if (package=="rmeta")
+
+if (type=="ignore")
 	{
-	meta_OR <- meta.DSL(exp_total, control_total, exp_events, control_events, data=myframe, names=Study)
-	tabletext<-cbind(c(NA,meta_OR$names,NA,"Summary","Heterogeneity p-value"),
-	c("Intervention",paste(myframe$exp_events,"/",myframe$exp_total),NA,NA,NA),
-	c("Control", paste(myframe$control_events,"/",myframe$control_total), NA,NA,NA),
-	c("OR",sprintf("%.2f",exp(meta_OR$logs)),NA,sprintf("%.2f",exp(meta_OR$logDSL)),sprintf("%.2f",meta_OR$het[3]))
-	)
-	m<- c(NA,meta_OR$logs,NA,meta_OR$logDSL)
-	l<- m-c(NA,meta_OR$selogs,NA,meta_OR$selogDSL)*2
-	u<- m+c(NA,meta_OR$selogs,NA,meta_OR$selogDSL)*2
-	forestplot(tabletext,m,l,u,zero=0,is.summary=c(TRUE,rep(FALSE,length(meta_OR$logs)),TRUE,TRUE),xlog=TRUE,clip=c(log(0.5),log(2)),xticks=c(0.5,1,2),
-	col=meta.colors(box="royalblue",line="darkblue", summary="royalblue"),xlab=c("       Favors intervention       Favors control"))
-	}
-else
-	{
-	#dev.new(width=10, height=7)
-	if (type=="ignore")
-		{
-		meta1 <- metabin(exp_events, exp_total, control_events,control_total, data=myframe, sm="OR", method="I", studlab=paste(Study))
-		}
-	if (type=="subgroup")
-		{
-		meta1 <- metabin(exp_events, exp_total, control_events,control_total, data=myframe, sm="OR", method="I", studlab=paste(Study),byvar=cofactor)
-		}
+	meta1 <- metabin(exp_events, exp_total, control_events,control_total, data=myframe, sm="OR", method="I", studlab=paste(Study))
 	#forest(meta1, leftcols="studlab",rightcols=FALSE, xlim=c(0.1, 10),ff.hetstat="plain",col.diamond="blue", col.diamond.lines="blue",comb.fixed=FALSE,print.tau2=FALSE)
 	forest(meta1, xlim=c(0.1, 10),ff.hetstat="plain",col.diamond="blue", col.diamond.lines="blue",comb.fixed=FALSE,print.tau2=FALSE)
+	}
+if (type=="subgroup")
+	{
+	meta1 <- metabin(exp_events, exp_total, control_events,control_total, data=myframe, sm="OR", method="I", studlab=paste(Study),byvar=cofactor)
+	#forest(meta1, leftcols="studlab",rightcols=FALSE, xlim=c(0.1, 10),ff.hetstat="plain",col.diamond="blue", col.diamond.lines="blue",comb.fixed=FALSE,print.tau2=FALSE)
+	forest(meta1, xlim=c(0.1, 10),ff.hetstat="plain",col.diamond="blue", col.diamond.lines="blue",comb.fixed=FALSE,print.tau2=FALSE)
+	}
+if (type=="metaregression")
+	{
+	meta1 <- meta.DSL(myframe[["exp_total"]], myframe[["control_total"]], myframe[["exp_events"]], myframe[["control_events"]],names=Study,conf.level=0.95)
+	studyweights <- 1 / (meta1$tau2 + meta1$selogs^2)
+	x <- myframe$cofactor
+	y <- meta1$logs
+	metaregression <- lm(y ~ x , data = myframe , weights = studyweights)
+	summary(metaregression)
+	plot(y ~ x, data = myframe, main=paste("Meta-regression of ", topic), xlab="", ylab="",ylim=c(-1,1),xaxs="r",xlim=c(40,130), type="n")
+	points(y ~ x,cex=10*studyweights/sum(studyweights),pch=21,bg='blue',col='blue')
+	text(x=x, y=y,labels=paste(Study), cex=0.65, pos=4,adj=0,font=1,col='black')
+	abline(h=0, v=0, col = "gray90")
+	abline(lm(y ~ x, data = myframe, weights = studyweights))
+	text(100,1,"Correlation of cofactor and odds ratio:",font=2,adj = c(0,0.5))
+	text(100,0.9,paste("All studies (" ,length(myframe$Study),"):",round(summary(metaregression)$coef[2,1],3),", p =",round(summary(metaregression)$coefficients[2,4],3)),adj = c(0,0.5))
+	mtext(side=1,line=2,paste("Cofactor",cofactorlabel), font=2)
+	mtext(side=2,line=3,"Odds ratio transformed to natural log (Ln)", font=2)
+	mtext(side=2,line=2,"(0 indicates odds ratio = 1)")
+	#mtext(side=3,line=0.5,"(Ln odds ratio below 0 favors treatment)")
+	mtext(side=1,line=3,cex=0.9,adj=0,"Notes:", font=2)
+	mtext(side=1,line=4,cex=0.9,adj=0, "1. For each study, the size of the point is its weight in the meta-regression.", font=1)
 	}
 #if(theme=="KU"){display_logo(x=1.2,y=0.05)}
 }
